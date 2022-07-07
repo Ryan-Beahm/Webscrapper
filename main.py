7445a1b60e558e506b169e97a6285c965776ea4f
Ryan Beahm <103221483+Ryan-Beahm@users.noreply.github.com>
2022-07-07 10:57:28 -0500

    project comments

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import requests
+#freecodecamp project
 import requests as rq
 from bs4 import BeautifulSoup as bs
 
 gitUser = input('Input github username: ')
 url = 'https://github.com/' + gitUser
 
-r = requests.get(url)
+# r stores the return code 200 and r.content stores the html file for further sorting
+r = rq.get(url)
+# test
+# print(r.content)
+
+# usable data formatting
+# print(bs(r.content, 'html.parser'))
 soup = bs(r.content, 'html.parser')
 
+# tag filtering
 profileImage = soup.find('img', {'alt': 'Avatar'})['src']
 print(profileImage)
